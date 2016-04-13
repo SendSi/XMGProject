4febbf8a12d4fbb3855f47bd7f8a9eb1c28fb8af
SendSi <scuplt@scupltdeMac.local>
2016-04-13 22:10:33 +0800

    Initial Commit

---
+++ XMGProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMGProject
+//
+//  Created by scuplt on 16-4-13.
+//  Copyright (c) 2016年 Scuplt0413. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ XMGProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMGProject
+//
+//  Created by scuplt on 16-4-13.
+//  Copyright (c) 2016年 Scuplt0413. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
